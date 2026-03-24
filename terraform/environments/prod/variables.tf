e5141e143f97f275174f730bd41aca039676d295
k3ozawa <k3ozawa@gmail.com>
2026-03-24 11:38:23 +0900

    feat: add Slack notification variables to prod environment

--- terraform/environments/prod/variables.tf
+++ terraform/environments/prod/variables.tf
@@ -1,10 +1,23 @@
 variable "aws_region" {
-  description = "AWSリージョン"
+  description = "AWS リージョン"
   type        = string
   default     = "ap-northeast-1"
 }
 
 variable "account_id" {
   description = "prod アカウント ID"
+  type        = string
+}
+
+# -------------------------------------------------------------------
+# Slack 通知設定
+# -------------------------------------------------------------------
+variable "slack_workspace_id" {
+  description = "Slack ワークスペース ID（AWS Chatbot コンソールで確認）"
+  type        = string
+}
+
+variable "slack_channel_id" {
+  description = "通知先 Slack チャンネル ID"
   type        = string
 }

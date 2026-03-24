1cd489550fa06ad6d529f4d88183ba479b2a2388
k3ozawa <k3ozawa@gmail.com>
2026-03-24 11:37:45 +0900

    feat: integrate slack-notifications into management environment

--- terraform/environments/management/main.tf
+++ terraform/environments/management/main.tf
@@ -27,3 +27,21 @@ module "cloudtrail" {
 
   depends_on = [module.organizations]
 }
+
+# -------------------------------------------------------------------
+# Slack 通知（コスト + セキュリティガバナンス）
+# -------------------------------------------------------------------
+module "slack_notifications" {
+  source = "../../modules/slack-notifications"
+
+  environment               = "management"
+  aws_region                = var.aws_region
+  account_id                = var.account_id
+  slack_workspace_id        = var.slack_workspace_id
+  slack_channel_id          = var.slack_channel_id
+  enable_cost_notifications = true
+  monthly_budget_amount     = var.monthly_budget_amount
+  cost_anomaly_threshold    = var.cost_anomaly_threshold
+
+  depends_on = [module.organizations]
+}

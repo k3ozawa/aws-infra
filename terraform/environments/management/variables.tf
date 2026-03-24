31d58cc85c7c91c5691fc2d6c684bd2d75f8461a
k3ozawa <k3ozawa@gmail.com>
2026-03-24 11:37:56 +0900

    feat: add Slack notification variables to management environment

--- terraform/environments/management/variables.tf
+++ terraform/environments/management/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
-  description = "AWSリージョン"
+  description = "AWS リージョン"
   type        = string
   default     = "ap-northeast-1"
 }
@@ -12,4 +12,36 @@ variable "dev_account_email" {
 variable "prod_account_email" {
   description = "prod アカウントのメールアドレス"
   type        = string
+}
+
+variable "account_id" {
+  description = "management アカウント ID"
+  type        = string
+}
+
+# -------------------------------------------------------------------
+# Slack 通知設定
+# AWS Chatbot コンソールで Slack ワークスペースを接続後に設定
+# https://console.aws.amazon.com/chatbot/
+# -------------------------------------------------------------------
+variable "slack_workspace_id" {
+  description = "Slack ワークスペース ID（AWS Chatbot コンソールで確認）"
+  type        = string
+}
+
+variable "slack_channel_id" {
+  description = "通知先 Slack チャンネル ID"
+  type        = string
+}
+
+variable "monthly_budget_amount" {
+  description = "月次予算アラートの閾値（USD）"
+  type        = number
+  default     = 10
+}
+
+variable "cost_anomaly_threshold" {
+  description = "コスト異常検知の閾値（USD）"
+  type        = number
+  default     = 5
 }
